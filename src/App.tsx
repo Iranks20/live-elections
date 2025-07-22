02f70783fc6b9aa0f76e76a0d290aa0d3dd87abf
Iranks20 <rankunda48@gmail.com>
2025-07-22 13:20:38 +0300

    jome

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import screens from './screens/index.js'
+import ElectionResults from './screens/ElectionResults'
 
 export default function App() {
 	const screenName =
@@ -8,25 +9,6 @@ export default function App() {
 		const Screen = screens[screenName as keyof typeof screens]
 		return <Screen />
 	}
-	// Default: show home (screen list)
-	return (
-		<div className="flex items-center justify-center h-screen text-gray-200 bg-gray-900">
-			<div className="flex flex-col gap-4">
-				<div className="text-4xl font-bold border-b-2 border-gray-700">
-					Project Screens
-				</div>
-				<div className="flex flex-col gap-2">
-					{Object.keys(screens).map(screenName => (
-						<a
-							key={screenName}
-							href={`/?screen=${screenName}`}
-							className="inline text-gray-300 transition-colors hover:text-indigo-400"
-						>
-							{screenName}
-						</a>
-					))}
-				</div>
-			</div>
-		</div>
-	)
+	// Default: show ElectionResults as the home page
+	return <ElectionResults />
 }
